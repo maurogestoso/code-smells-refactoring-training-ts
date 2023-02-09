7830a89c4cc14b1f8fb0ccc39da0b342fec9eee6
Mauro <maurogestoso@gmail.com>
2023-02-09 10:53:54 +0100

    Refactor: extract displacement functionality to Coordinates class
    Co-authored-by: Javier Baena <JvrBaena@users.noreply.github.com>

--- 01-refactoring-smelly-mars-rover/src/Coordinates.ts
+++ 01-refactoring-smelly-mars-rover/src/Coordinates.ts
@@ -1,13 +1,19 @@
 export class Coordinates {
-    constructor(private x: number, private y: number) {
+  constructor(private x: number, private y: number) {}
 
-    }
+  getX() {
+    return this.x;
+  }
 
-    getX() {
-        return this.x;
-    }
+  getY() {
+    return this.y;
+  }
 
-    getY() {
-        return this.y;
-    }
+  moveAlongY(displacement: number) {
+    return new Coordinates(this.x, this.y + displacement);
+  }
+
+  moveAlongX(displacement: number) {
+    return new Coordinates(this.x + displacement, this.y);
+  }
 }
--- 01-refactoring-smelly-mars-rover/src/Rover.ts
+++ 01-refactoring-smelly-mars-rover/src/Rover.ts
@@ -1,74 +1,66 @@
 // @ts-nocheck
 
-import {Direction} from "./Direction";
-import {Coordinates} from "./Coordinates";
+import { Direction } from "./Direction";
+import { Coordinates } from "./Coordinates";
 
 export class Rover {
-    
-    private directionType: Direction;
-    private coordinates: Coordinates;
+  private directionType: Direction;
+  private coordinates: Coordinates;
 
-    constructor(x: number, y: number, direction: string) {
-        this.setCoordinates(x, y);
-        this.setDirection(direction);
-    }
+  constructor(x: number, y: number, direction: string) {
+    this.coordinates = new Coordinates(x, y);
+    this.setDirection(direction);
+  }
 
-    private setCoordinates(x: number, y: number) {
-        this.coordinates = new Coordinates(x, y)
-    }
+  private setDirection(direction: string) {
+    this.directionType = new Direction(direction);
+  }
 
-    private setDirection(direction: string) {
-        this.directionType = new Direction(direction);
-    }
+  public receive(commandsSequence: string) {
+    for (let i = 0; i < commandsSequence.length; ++i) {
+      const command = commandsSequence.substring(i, i + 1);
 
-    public receive(commandsSequence: string) {
-        for (let i = 0; i < commandsSequence.length; ++i) {
-            const command = commandsSequence.substring(i, i + 1);
-
-            if (command === "l") {
-
-                // Rotate Rover left
-                if (this.directionType.isFacingNorth()) {
-                    this.setDirection("W");
-                } else if (this.directionType.isFacingSouth()) {
-                    this.setDirection("E");
-                } else if (this.directionType.isFacingWest()) {
-                    this.setDirection("S");
-                } else {
-                    this.setDirection("N");
-                }
-            } else if (command === "r") {
-
-                // Rotate Rover right
-                if (this.directionType.isFacingNorth()) {
-                    this.setDirection("E");
-                } else if (this.directionType.isFacingSouth()) {
-                    this.setDirection("W");
-                } else if (this.directionType.isFacingWest()) {
-                    this.setDirection("N");
-                } else {
-                    this.setDirection("S");
-                }
-            } else {
-
-                // Displace Rover
-                let displacement1 = -1;
-
-                if (command === "f") {
-                    displacement1 = 1;
-                }
-                let displacement = displacement1;
-
-                if (this.directionType.isFacingNorth()) {
-                    this.setCoordinates(this.coordinates.getX(), this.coordinates.getY() + displacement);
-                } else if (this.directionType.isFacingSouth()) {
-                    this.setCoordinates(this.coordinates.getX(), this.coordinates.getY() - displacement);
-                } else if (this.directionType.isFacingWest()) {
-                    this.setCoordinates(this.coordinates.getX() - displacement, this.coordinates.getY());
-                } else {
-                    this.setCoordinates(this.coordinates.getX() + displacement, this.coordinates.getY());
-                }
-            }
+      if (command === "l") {
+        // Rotate Rover left
+        if (this.directionType.isFacingNorth()) {
+          this.setDirection("W");
+        } else if (this.directionType.isFacingSouth()) {
+          this.setDirection("E");
+        } else if (this.directionType.isFacingWest()) {
+          this.setDirection("S");
+        } else {
+          this.setDirection("N");
         }
+      } else if (command === "r") {
+        // Rotate Rover right
+        if (this.directionType.isFacingNorth()) {
+          this.setDirection("E");
+        } else if (this.directionType.isFacingSouth()) {
+          this.setDirection("W");
+        } else if (this.directionType.isFacingWest()) {
+          this.setDirection("N");
+        } else {
+          this.setDirection("S");
+        }
+      } else {
+        // Displace Rover
+        let displacement1 = -1;
+
+        if (command === "f") {
+          displacement1 = 1;
+        }
+        let displacement = displacement1;
+
+        if (this.directionType.isFacingNorth()) {
+          this.coordinates = this.coordinates.moveAlongY(displacement);
+        } else if (this.directionType.isFacingSouth()) {
+          this.coordinates = this.coordinates.moveAlongY(-displacement);
+        } else if (this.directionType.isFacingWest()) {
+          this.coordinates = this.coordinates.moveAlongX(-displacement);
+        } else {
+          this.coordinates = this.coordinates.moveAlongX(displacement);
+        }
+      }
     }
+  }
 }
